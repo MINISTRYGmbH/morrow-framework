275108f65db637009c3052bf93b78e75e38abc94
Christoph Erdmann <noreply@users.noreply.github.com>
2014-06-15 21:26:40 +0200

    Initial

--- public/index.php
+++ public/index.php
@@ -1,19 +1,7 @@
 <?php
 
-namespace Morrow;
-
-// define paths
-define('PUBLIC_PATH', realpath(__DIR__) . '/');
-define('PUBLIC_STORAGE_PATH', realpath(__DIR__ . '/storage') . '/');
-define('APP_PATH', realpath(__DIR__ . '/../app') . '/');
-define('STORAGE_PATH', realpath(__DIR__ . '/../app/storage') . '/');
-
-// define the path to vendor dir
-// change this if you have to projects which should use the same vendor folder
-define('VENDOR_PATH', realpath(__DIR__ . '/../vendor') . '/');
-
 // register the Composer autoloader
-require VENDOR_PATH . 'autoload.php';
+require '../vendor/autoload.php';
 
 // execute Morrow framework
-new Core\Frontcontroller();
+new Morrow\Core\Bootstrap();
