a57859904e41829a673075462f8b74dda6c278fc
Christoph Erdmann <McSodbrenner@users.noreply.github.com>
2014-06-17 11:14:55 +0200

    .

--- App/Home.php
+++ App/Home.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace app;
 use Morrow\Factory;
 use Morrow\Debug;
 
--- App/models/Example.php
+++ App/models/Example.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace app\models;
 use Morrow\Debug;
 use Morrow\Factory;
 
