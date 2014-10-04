a812f41ef014bd0db521a9c32e2064e5fcfbe7f4
Christoph Erdmann <noreply@users.noreply.github.com>
2014-10-04 13:29:04 +0200

    Added starter page

--- app/configs/_default_app.php
+++ app/configs/_default_app.php
@@ -3,15 +3,14 @@
 // If you want to modify defaults for your project (like routing rules), use this file.
 
 return array(
-// security
-	'security.csp.default-src'		=> "'self'",
+	'security.csp.default-src'		=> "'self' cdnjs.cloudflare.com",
 	// 'security.csp.script-src'	=> "'self'",
 	// 'security.csp.img-src'		=> "'self'",
-	'security.csp.style-src'		=> "'self' 'unsafe-inline' fonts.googleapis.com",
+	'security.csp.style-src'		=> "'self' 'unsafe-inline' cdnjs.cloudflare.com",
 	// 'security.csp.media-src'		=> "'self'",
 	// 'security.csp.object-src'	=> "'self'",
 	// 'security.csp.frame-src'		=> "'self'",
-	'security.csp.font-src'			=> "'self' fonts.gstatic.com",
+	// 'security.csp.font-src'		=> "'self'",
 	'security.frame_options'		=> "DENY", // (DENY|SAMEORIGIN|ALLOW-FROM uri)
 	'security.content_type_options'	=> "nosniff",
 );
